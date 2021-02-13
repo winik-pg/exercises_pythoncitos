9880dc4b540bf892014cf72fce3813f75002eb59
Felipe de Alba <45267327+fdealbam@users.noreply.github.com>
2021-02-12 19:54:30 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,13 @@ import pandas as pd
 
 
 ################################### Abre archivos
-zacatecas = pd.read_csv('https://raw.githubusercontent.com/Aeelen-Miranda/flying-dog-beers/master/Tabla%202.%20Delitos%20Zacatecas%20(2020)_2.csv', )
-covid = pd.read_csv("https://raw.githubusercontent.com/Aeelen-Miranda/flying-dog-beers/master/cdmx_deaths.csv")
+zacatecas = pd.read_csv('https://raw.githubusercontent.com/fdealbam/flying-dog-beers/master/Tabla%202.%20Delitos%20Zacatecas%20(2020)_2.csv', )
+covid = pd.read_csv("https://raw.githubusercontent.com/fdealbam/flying-dog-beers/master/cdmx_deaths.csv")
+
+
 
 ################################### Prepara Grafica 1
 pv = pd.pivot_table(zacatecas, index=['Municipio'], columns=['Tipo de delito'], values=['ene-20'],aggfunc=sum, fill_value = 0)
-
 
 g1 = go.Bar(x=pv.index, y=pv[('ene-20', 'Robo')], name = 'Robo')
 gr1 = go.Bar(x=pv.index, y=pv[('ene-20', 'Violencia familiar')], name = 'Violencia familiar')
@@ -24,12 +25,15 @@ grafi1 = go.Bar(x=pv.index, y=pv[('ene-20', 'Daño a la propiedad')], name = 'Da
 grafic1 = go.Bar(x=pv.index, y=pv[('ene-20', 'Fraude')], name = 'Fraude')
 grafica1 = go.Bar(x=pv.index, y=pv[('ene-20', 'Amenazas')], name = 'Amenazas')
 
+
 ################################## Grafica 2
 grafica2 = px.line(covid, x = covid['nom_mun'], y = covid['quincena0'])
 grafica2.update_traces(orientation = 'v')
 grafica2.update_layout(font_family="Montserrat",title = '<b>Quincena 0</b>',
                        template = 'presentation',title_font_family="Montserrat",
                        title_font_color="goldenrod",)
+
+
 #################################### Grafica 3
 grafica3 = px.line(covid, x = covid['nom_mun'], y = covid['quincena2'])
 grafica3.update_traces(orientation = 'v')
@@ -38,12 +42,16 @@ grafica3.update_layout(font_family="Montserrat",
                        template = 'presentation',
                        title_font_family="Montserrat",
                        title_font_color="goldenrod",)
+
+
 ################################### Grafica 4
 grafica4 = px.line(covid, x = covid['nom_mun'], y = covid['quincena3'])
 grafica4.update_traces(orientation = 'v')
 grafica4.update_layout(font_family="Montserrat",title = '<b>Quincena 3</b>',
                       template='presentation',title_font_family="Montserrat",
                       title_font_color="goldenrod",)
+
+
 ############################################ Grafica 5
 grafica5 = px.line(covid, x = covid['nom_mun'], y = covid['quincena4'])
 grafica5.update_traces(orientation = 'v')
@@ -51,6 +59,7 @@ grafica5.update_layout(font_family="Montserrat",title = '<b>Quincena 4</b>',
                       template='presentation',title_font_family="Montserrat",
                        title_font_color="goldenrod", )
 grafica5.update_xaxes(title_font_family="Montserrat")
+
 
 ############################################ Grafica 6
 grafica6 = px.line(covid, x = covid['nom_mun'], y = covid['quincena5']) # , color= "nom_mun")
@@ -68,6 +77,8 @@ grafica6.update_layout(font_family="Montserrat", #Tipo de letra del contenido de
                       #line_color= "dark"
                       ) #Con esto se cambia color letra
 grafica6.update_xaxes(title_font_family="Montserrat") #Tipo de letra de x,y
+
+
 ########################################### Grafica 8
 grafica8 = px.scatter(covid, x='Total', y='nom_mun', size='Total', 
                       color='quincena6', title = '<b>Incidencia delictiva en alcaldias</b>',
@@ -86,6 +97,7 @@ grafica8.update_layout(
 grafica8.update_xaxes(title_font_family="Montserrat")
 #PRIMEr paso
 
+
 ############################################ Grafica 9
 eindex = covid[['nom_mun','Total' ,'quincena0', 'quincena2',
        'quincena3', 'quincena4', 'quincena5', 'quincena6']]
@@ -101,15 +113,12 @@ egroup_index = eindexx[['Total']].sort_values(by = 'nom_mun')
 egroup_index
 
 # creación de la gráfica 
-
-
 grafica9 = go.Figure()
 grafica9.add_trace(go.Bar(
     y=eindexx.quincena0.values,
     x=eindexx.index,
     name='quincena0',
     marker_color='#572364',
-     
 ))
 
 grafica9.add_trace(go.Bar(
@@ -143,20 +152,21 @@ grafica9.add_trace(go.Bar(
     name='quincena6',
     marker_color='#CE93D8'
 ))
-
 grafica9.update_traces(orientation = 'v')
 grafica9.update_layout(title = '<b>9. Delitos totales por Alcaldía</b>',
                        title_font_family="Montserrat",title_font_color="goldenrod",
                        
                  template='presentation')
+
+
+
 ####################################################################################
 ########### Define your variables
 
 mytitle='Añadir graficas'
 tabtitle='Prueba Dash!'
-githublink='https://github.com/Aeelen-Miranda/flying-dog-beers'
+githublink='https://github.com/fdealbam/flying-dog-beers'
 sourceurl='https://plotly.com/python/histograms/'
-
 
 
 ########### Initiate the app
@@ -173,8 +183,8 @@ app.layout = html.Div(children=[
     
     html.Div(children = [ dcc.Markdown(
         ''' 
-    #Prueba de Dashboard
-    ## prueba sobre delitos
+    ### *Dashboard mejorado*
+    # Incidencia de delitos
     ###### jueves 11 de febrero de 2020
 ''',
          )],style={'font-family': 'Montserrat',# 'sans-serif',
@@ -189,22 +199,26 @@ app.layout = html.Div(children=[
                                            plot_bgcolor='white',
                                            title='Mayor incidencia delictiva',
                                            barmode='group')})],
-             style = {'margin': '1% 0px 0px 0px', 'width':'60%',
+             style = {'margin': '1% 0px 0px 0px', 'width':'100%',
                      'font-family': 'Montserrat', 
                       'fontColor': 'white' #Cambia tipo de letra
                     }),
+  
    html.Div(children =[dcc.Graph(figure=grafica2)],
-            style={'margin': '2% 0px 0px 1px', 'width':'50%',
+            style={'margin': '2% 0px 0px 1px', 'width':'20%',
                   'font-family': 'Montserrat',
                   'backgroundColor': colors['background']}),
+  
    html.Div(children = [dcc.Graph(figure=grafica3)],
-           style={'margin': '2% 0px 0px 1px', 'width':'50%',
+           style={'margin': '2% 0px 0px 1px', 'width':'20%',
                  'font-family': 'Montserrat',
                  'backgroundColor': colors['background']}),
+  
     html.Div(children =[dcc.Graph(figure=grafica4)],
-             style={'margin': '2% 0px 0px 1px', 'width':'50%',
+             style={'margin': '2% 0px 0px 1px', 'width':'20%',
                    'font-family': 'Montserrat',
                    'backgroundColor': colors['background']}),
+  
     html.Div(children = [dcc.Graph(figure=grafica5)],
             style={'margin': '2% 0px 0px 0px', 'width':'22%',
                   'font-family': 'Montserrat',
@@ -214,16 +228,20 @@ app.layout = html.Div(children=[
              style={'margin': '3% 0px 0px 0px', 'width':'100%',
                    'font-family': 'Montserrat',
                    'backgroundColor': colors['background']}),
+  
     html.Div(children =[dcc.Graph(figure=grafica8)],
-            style={'margin': '2% 0px 0px 0px', 'width':'60%',
+            style={'margin': '4% 0px 0px 0px', 'width':'40%',
                   'font-family': 'Montserrat',
                   'backgroundColor': colors['background']}),
 
 #quinta franja
     html.Div(children = [dcc.Graph(figure=grafica9)],
-            style={'margin': '2% 0px 0px 0px', 'width':'100%',
+            style={'margin': '4% 0px 0px 0px', 'width':'40%',
                   'font-family': 'Montserrat',
                   'backgroundColor': colors['background']}),
+  
+  
+  
     html.A('Code on Github', href=githublink),
     html.Br(),
     html.A('Data Source', href=sourceurl),
