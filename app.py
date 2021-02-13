aaf05538b24e47aca07945568397e4fa95fb7622
Felipe de Alba <45267327+fdealbam@users.noreply.github.com>
2021-02-13 16:01:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -257,9 +257,39 @@ app.layout = html.Div(children=[
         ),
      
 
-    
-    
-   
+##########    
+            #html.Div([Header(app)]),
+            # page 1
+            #html.Div(
+            #    [
+                    # Row 3
+                    html.Div(children= 
+                        [
+                            html.Div(
+                                [
+                                    html.H5("Product Summary"),
+                                    html.Br([]),
+                                    html.P(
+                                        "\
+                                    As the industry’s first index fund for individual investors, \
+                                    the Calibre Index Fund is a low-cost way to gain diversified exposure \
+                                    to the U.S. equity market. The fund offers exposure to 500 of the \
+                                    largest U.S. companies, which span many different industries and \
+                                    account for about three-fourths of the U.S. stock market’s value. \
+                                    The key risk for the fund is the volatility that comes with its full \
+                                    exposure to the stock market. Because the Calibre Index Fund is broadly \
+                                    diversified within the large-capitalization market, it may be \
+                                    considered a core equity holding in a portfolio.",
+                                        style={"color": "#ffffff"},
+                                        className="row",
+                                    ),
+                                ],
+                                className="product",
+                            )
+                        ],
+                        className="row",
+                    ),
+##############   
     
     html.Div( children = [dcc.Graph(id='grafica1',
               figure= {'data':[g1,gr1,gra1,graf1,grafi1,grafic1,grafica1],
